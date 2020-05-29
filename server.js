3f59488f9b38b9fb8fb7875313bc427db81a56f7
pechaprasanna <rpv31dec1991@gmail.com>
2020-05-29 10:21:37 -0700

    clarifai body fix

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfile(req, res, db) })
 app.put('/image', (req, res) => { image.handleImage(req, res, db) })
-app.put('/imageurl', (req, res) => { image.handleAPICall(req, res) })
+app.put('/imageapi', (req, res) => { image.handleAPICall(req, res) })
 
 
 
