a6f34d5e1f86a426c6e71bdc001a23107294379f
pechaprasanna <rpv31dec1991@gmail.com>
2020-06-02 04:48:10 -0700

    deploying in openshift

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image')
 const db = knex({
   client: 'pg',
   connection: {
-    connectionString: 'postgres://postgres_user:postgres_password@db/postgres'
+    connectionString: 'postgres://postgres_username:postgres_password@db/postgres'
   }
 });
 
