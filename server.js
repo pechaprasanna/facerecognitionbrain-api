ac5d267d7a4330f5f0193afcc9b75b9712358bc2
pechaprasanna <rpv31dec1991@gmail.com>
2020-06-02 05:00:11 -0700

    deploying in openshift

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const image = require('./controllers/image')
 
 const db = knex({
   client: 'pg',
-  connection: 'postgres://postgres_username:postgres_password@db/postgres'
+  connection: 'postgres://postgres_username:postgres_password@db:5432/postgres'
 });
 
 app.use(express.json());
